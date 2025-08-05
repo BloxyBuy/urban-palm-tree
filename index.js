74182f6484871cb67b60c19ceeba26b16f29ac0a
BloxyBuy <kbercicreator@freemail.hu>
2025-08-05 11:54:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
+const express = require('express');
+const app = express();
 
+// Respond to root URL to show the bot is alive
+app.get('/', (req, res) => {
+  res.send('I am alive!');
+});
+
+// Use the port Render gives you or fallback to 3000 locally
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Express server running on port ${PORT}`);
+});
 
 const {mineboty} = require("mineboty")
 mineboty();
