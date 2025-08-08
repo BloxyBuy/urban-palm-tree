377d5d6cc33173495cf1f078a9f15d7f0733d7a3
BloxyBuy <kbercicreator@freemail.hu>
2025-08-08 10:47:42 +0200

    Update index.js

--- mineboty/index.js
+++ mineboty/index.js
@@ -1,51 +1,55 @@
-﻿/* 
+/* 
 © 2021 by Mineboty 
 © Team IC
 Author - Kartik
-
 */
 
 function mineboty() {
     console.log("\x1B[36m \x1B[1m Welcome to mineboty v1.0.6 see :- \x1B[31m https://www.npmjs.com/package/mineboty");
     console.log("\x1B[35m \x1B[1mJoin discord :- \x1B[31m https://teamic.ml/");
     console.log("\x1B[31m You have any problem or issues you can see this & frok also :) :- \x1B[32m[30m https://github.com/Team-IC/Mineboty");
-    return require("./runnerboty/runnerboty")
+
+    // Require the bot runner module
+    const botRunner = require("./runnerboty/runnerboty");
+
+    // Call the returned function to actually start the bot
+    if (typeof botRunner === "function") {
+        botRunner();
+    } else {
+        console.error("Error: runnerboty did not export a function!");
+    }
 }
 
-
 function online() {
-    return require("./online")
+    return require("./online");
 }
 
 function eat() {
-    return require("./eat")
+    return require("./eat");
 }
 
 function cmd() {
-    return require("mineflayer-cmd")
+    return require("mineflayer-cmd");
 }
 
 function pvp() {
-    return require("./pvp")
+    return require("./pvp");
 }
 
 function inv() {
-    return require("./runnermodules/runwebinv")
+    return require("./runnermodules/runwebinv");
 }
 
 function ne() {
-    return require("./runnermodules/new")
+    return require("./runnermodules/new");
 }
 
-
 function mcc() {
-    return require("./mcc")
+    return require("./mcc");
 }
 
 function paste() {
-    return require("./paste")
+    return require("./paste");
 }
-
-
 
 module.exports = { mineboty, online, eat, pvp, inv, cmd, ne, paste, mcc };
