9f0fa7b04e0c57f34f959607c2a38858599ca8c4
BloxyBuy <kbercicreator@freemail.hu>
2025-08-09 12:06:10 +0200

    Update index.js

--- mineboty/index.js
+++ mineboty/index.js
@@ -8,7 +8,7 @@ function mineboty() {
     console.log("\x1B[36m \x1B[1m Welcome to mineboty v1.0.6 see :- \x1B[31m https://www.npmjs.com/package/mineboty");
     console.log("\x1B[35m \x1B[1mJoin discord :- \x1B[31m https://teamic.ml/");
     console.log("\x1B[31m You have any problem or issues you can see this & frok also :) :- \x1B[32m[30m https://github.com/Team-IC/Mineboty");
-    require("./runnerboty/runnerboty");  // ← ✅ just require it, no return
+    const botRunner = require("./runnerboty/runnerboty");
 
     // Call the returned function to actually start the bot
     if (typeof botRunner === "function") {
@@ -51,5 +51,6 @@ function paste() {
 }
 
 module.exports = { mineboty, online, eat, pvp, inv, cmd, ne, paste, mcc };
+
 
 
